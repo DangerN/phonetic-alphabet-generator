d1a556c47abd80f5cc1564a4f15a19eb35134289
Crystal Danger <cdneal091@gmail.com>
2020-01-09 14:28:34 -0500

    rerender not working after axios call

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React, { useState } from 'react'
+import React, { useState, useReducer } from 'react'
 import { Container, Col, Row, Button, Form } from 'react-bootstrap'
+import axios from 'axios'
 
 import AlphaTable from './components/AlphaTable'
-import {Alphabet} from './constants.js'
-
+import {alphabet} from './constants.js'
 
 export default () => {
-  const [wordOptions, setWordOptions] = useState()
+  const wordOptions = {}
   const [input, setInput] = useState("")
-  const handleGenerate = () => {
+  const reducer = (state, action) => {
+    newState[action.index] = action.word
+    return newState
+  }
+  const [generated, dispatch] = useReducer(reducer, [""])
+  const handleGenerate = event => {
+    console.log(dispatch);
+    console.log(generated);
+    event.preventDefault()
+    alphabet.char.map((letter, index) =>{
+      axios.get(`http://api.datamuse.com/words/?sp=${letter}*&topics=${input}`).then(res=>{
+        wordOptions[letter] = res.data
+        alphabet.generated[index] = res.data[0].word
+        dispatch({word: res.data[0].word, index: index})
+      })
+    })
   }
   const handleInputChange = event => {
     setInput(event.target.value)
   }
+  console.log('render');
   return (
     <div className="App" >
       <Container>
         <Row>
           <Col md={6}>
-            <AlphaTable alphabet={Alphabet} />
+            <AlphaTable alphabet={alphabet} generated={generated} />
           </Col>
           <Col>
             <Form>
--- src/components/AlphaTable.jsx
+++ src/components/AlphaTable.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import Table from 'react-bootstrap/Table'
 
 export default props => {
-  const { char, NATO, generated } = props.alphabet
+  const { char, NATO } = props.alphabet
   const tableBody = () => {
     return char.map((letter, index)=>{
       return (<tr key={letter}>
         <td>{letter}</td>
         <td>{NATO[index]}</td>
-        <td>{generated[index] ? generated[index] : "Yeed"}</td>
+        <td>{props.generated[index]}</td>
       </tr>)
     })
   }
