6af197bda40e6480bbf7d772e114bd8c16f498e1
Crystal Danger <cdneal091@gmail.com>
2020-01-12 18:54:53 -0500

    Updates

---
+++ precache-manifest.b1ef2b62fe31ef0c076a24c34094b995.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e946efc05cbe6bc499a17facca9eaa1",
+    "url": "/phonetic-alphabet-generator/index.html"
+  },
+  {
+    "revision": "c56f3e0e3665a17021a9",
+    "url": "/phonetic-alphabet-generator/static/css/2.ead1b15b.chunk.css"
+  },
+  {
+    "revision": "c56f3e0e3665a17021a9",
+    "url": "/phonetic-alphabet-generator/static/js/2.c4468c46.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/phonetic-alphabet-generator/static/js/2.c4468c46.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fa16c300420eae5aa405",
+    "url": "/phonetic-alphabet-generator/static/js/main.4356aa5b.chunk.js"
+  },
+  {
+    "revision": "6c019b3296d5db9441bb",
+    "url": "/phonetic-alphabet-generator/static/js/runtime-main.fabd37ae.js"
+  }
+]);
